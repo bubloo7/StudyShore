b163ef5f1bb5e4c0c20876f45159354cf401d8aa
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2024-04-06 14:45:31 -0700

    bug fixes

--- backend/backend.py
+++ backend/backend.py
@@ -2,6 +2,10 @@ from flask import Flask, request
 from flask_cors import CORS
 import os
 from docx import Document
+from pypdf import PdfReader
+import win32com.client
+
+
 
 app = Flask(__name__)
 CORS(app)
@@ -14,25 +18,48 @@ def handle_jpeg(jpeg):
     pass
 
 def handle_pdf(pdf):
-    pass
+    reader = PdfReader(os.getcwd() + '/file.pdf')
+    number_of_pages = len(reader.pages)
+    s = ''
+    for i in range(number_of_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+        s += text
+    
+    print(s)
+    # pass in s to gemini
 
 def handle_txt(txt):
     pass
 
 def handle_docx(docx):
-    d = Document(os.getcwd() + '/file')
+    d = Document(os.getcwd() + '/file.docx')
     s = ''
     for paragraph in d.iter_inner_content():
         for line in paragraph.iter_inner_content():
-            s += line
+            s += line.text
         s += '\n'
     
+    print(s)
     # pass in s to gemini
 
 def handle_mp3(mp3):
     pass
 
 def handle_mp4(mp4):
+    pass
+
+def handle_pptx(pptx):
+    powerpoint = win32com.client.DispatchEx("Powerpoint.Application")
+    powerpoint.Visible = 1
+    deck = powerpoint.Presentations.Open('file.pptx')
+    deck.SaveAs("file.pdf", 32)
+    deck.Close()
+    powerpoint.Quit()
+
+
+
+
     pass
 
 @app.route('/')
@@ -43,9 +70,10 @@ def hello_world():
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
-    file.save(dst = os.getcwd() + '/file')
     name = file.filename
     extension = name.split('.')[-1]
+    file.save(dst = os.getcwd() + '/file.' + extension)
+
     print(extension, name)
     if extension == 'png':
         handle_png(file)
@@ -61,6 +89,8 @@ def upload():
         handle_mp4(file)
     elif extension == 'mp4':
         handle_mp3(file)
+    elif extension == 'pptx':
+        handle_pptx(file)
 
     print('pinged', file, type(file), file.filename)
 
