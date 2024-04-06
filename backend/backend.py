e7c7533692ff0a2408e0cbd0e15d9fc22cf92b8d
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2024-04-06 13:53:33 -0700

    handle docx

--- backend/backend.py
+++ backend/backend.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import os
-
+from docx import Document
 
 app = Flask(__name__)
 CORS(app)
@@ -20,8 +20,20 @@ def handle_txt(txt):
     pass
 
 def handle_docx(docx):
+    d = Document(os.getcwd() + '/file')
+    s = ''
+    for paragraph in d.iter_inner_content():
+        for line in paragraph.iter_inner_content():
+            s += line
+        s += '\n'
+    
+    # pass in s to gemini
+
+def handle_mp3(mp3):
     pass
 
+def handle_mp4(mp4):
+    pass
 
 @app.route('/')
 def hello_world():
@@ -31,6 +43,7 @@ def hello_world():
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
+    file.save(dst = os.getcwd() + '/file')
     name = file.filename
     extension = name.split('.')[-1]
     print(extension, name)
@@ -44,6 +57,10 @@ def upload():
         handle_txt(file)
     elif extension == 'docx':
         handle_docx(file)
+    elif extension == 'mp3':
+        handle_mp4(file)
+    elif extension == 'mp4':
+        handle_mp3(file)
 
     print('pinged', file, type(file), file.filename)
 
