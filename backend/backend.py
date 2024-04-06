afa2f423a9922b9323b63effc393e35de2f1633e
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2024-04-06 13:25:02 -0700

    skeleton code for handeling input

--- backend/backend.py
+++ backend/backend.py
@@ -2,8 +2,26 @@ from flask import Flask, request
 from flask_cors import CORS
 import os
 
+
 app = Flask(__name__)
 CORS(app)
+
+
+def handle_png(png):
+    pass
+
+def handle_jpeg(jpeg):
+    pass
+
+def handle_pdf(pdf):
+    pass
+
+def handle_txt(txt):
+    pass
+
+def handle_docx(docx):
+    pass
+
 
 @app.route('/')
 def hello_world():
@@ -13,8 +31,22 @@ def hello_world():
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
-    print('pinged', file, type(file))
-    file.save(dst= os.getcwd() + '/file')
+    name = file.filename
+    extension = name.split('.')[-1]
+    print(extension, name)
+    if extension == 'png':
+        handle_png(file)
+    elif extension == 'jpg':
+        handle_jpeg(file)
+    elif extension == 'pdf':
+        handle_pdf(file)
+    elif extension == 'txt':
+        handle_txt(file)
+    elif extension == 'docx':
+        handle_docx(file)
+
+    print('pinged', file, type(file), file.filename)
+
     return {"file recieved": True}
 
 
