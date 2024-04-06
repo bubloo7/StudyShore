904c5c0b9e3288a789d525308037d992a0ecc4e9
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2024-04-06 13:08:06 -0700

    Added shadcn upload button + backend handles upload and saves file

--- backend/backend.py
+++ backend/backend.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    print('pinged', file, type(file))
+    file.save(dst= os.getcwd() + '/file')
+    return {"file recieved": True}
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
