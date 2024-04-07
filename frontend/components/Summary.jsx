fa4db91df2f07d7184d467efbb9552badbe10685
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2024-04-07 12:52:15 -0700

    Update Summary.jsx

--- frontend/components/Summary.jsx
+++ frontend/components/Summary.jsx
@@ -3,8 +3,21 @@ import remarkGfm from "remark-gfm";
 
 export default function Summary({ data }) {
     return (
-        <div>
-            <ReactMarkdown children={data} remarkPlugins={[remarkGfm]} />
+        <div
+            style={{
+                textAlign: "left",
+                fontSize: "16px",
+                lineHeight: "2.0",
+                marginBottom: "30px",
+                paddingLeft: "50px",
+            }}
+        >
+            <style>{`
+            strong {
+                font-size: 24px; /* Adjust the font size as desired */
+                font-weight: bold; /* Ensure bold weight */
+            }`}</style>
+            <ReactMarkdown children={data} />
         </div>
     );
 }
